635b17bf218eabf044b21a42cc7d02a171a4a025
terminator0x <mulish.tc@gmail.com>
2021-03-02 12:28:55 +0300

    truffle config changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,18 @@
-const truffleTeamsUrl = "https://sandbox.truffleteams.com/4c1224c1-3a9c-43ad-9f1e-0eecb9e8995b"
+const truffleTeamsUrl = "https://sandbox.truffleteams.com/4c1224c1-3a9c-43ad-9f1e-0eecb9e8995b";
 
-export const networks = {
-  teams: {
-    url: truffleTeamsUrl,
-    network_id: 1,
+module.exports = {
+  networks: {
+    "teams": {
+      url: truffleTeamsUrl,
+      network_id: 1,
+    },
   },
-};
-export const mocha = {
-   timeout: 100000
-};
-export const compilers = {
-  solc: {
-    version: '0.7.4',
+  mocha: {
+    timeout: 100000
+  },
+  compilers: {
+    solc: {
+      version: '0.7.4',
+    }
   }
-};
+}

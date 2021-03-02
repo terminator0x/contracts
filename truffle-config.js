9c344387060f361cdd974331e70e488c262fedc3
terminator0x <mulish.tc@gmail.com>
2021-03-02 16:02:45 +0300

    increased testing gas price

--- truffle-config.js
+++ truffle-config.js
@@ -5,17 +5,24 @@ module.exports = {
     teams: {
       url: truffleTeamsUrl,
       network_id: 1,
-      gas: 550000000000,
+      gas: 950000000000,
       confirmations: 1,
       timeoutBlocks: 200
     },
   },
   mocha: {
-    timeout: 100000
+    timeout: 100000,
+    useColors: true
   },
   compilers: {
     solc: {
       version: '0.7.4',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
     }
   }
 }
